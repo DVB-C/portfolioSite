66af6d37a24cdfc596902f62d6118dd19bffcfe6
kalpitp <kalpit.parikh@outlook.com>
2019-09-06 16:11:14 -0400

    version 2.0

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
-$("#target1").click(function() {
-  window.location.href = "#home"
-});
-
-$("#target2").click(function() {
-  window.location.href = "#aboutus"
-});
-
-$("#target3").click(function() {
-  window.location.href = "#portfolio"
-});
-
-$("#target4").click(function() {
-  window.location.href = "#contact-area"
-});
-
-$("#contact").click(function() {
-  window.location.href = "mailto:kalpit.parikh@outlook.com"
-});
-
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 1) {
-    $('header').addClass("sticky");
+$( '.js-input' ).keyup(function() {
+  if( $(this).val() ) {
+     $(this).addClass('not-empty');
   } else {
-    $('header').removeClass("sticky");
+     $(this).removeClass('not-empty');
   }
 });
